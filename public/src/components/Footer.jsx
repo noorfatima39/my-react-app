ee30b7d6b60a61bd2ed89c09b28f4d22a6408a8e
noorfatima39 <noorfatima39@example.com>
2026-05-21 13:17:40 +0500

    update footer

--- public/src/components/Footer.jsx
+++ public/src/components/Footer.jsx
@@ -1,36 +1,31 @@
 import React from "react";
 
 export default function Footer() {
-
   return (
     <footer className="footer">
 
+      {/* Brand Section */}
       <div>
-        <h3>My Website</h3>
-
+        <h3>DevSphere</h3>
         <p>
-          Professional React UI Website
+          Crafting modern web experiences with clean engineering and premium user interfaces.
         </p>
       </div>
 
+      {/* Quick Links Section */}
       <div>
         <h4>Quick Links</h4>
-
-        <p>Home</p>
-
-        <p>Posts</p>
-
-        <p>Contact</p>
+        <p><a href="/" style={{ color: "inherit", textDecoration: "none" }}>Home</a></p>
+        <p><a href="/posts" style={{ color: "inherit", textDecoration: "none" }}>Posts</a></p>
+        <p><a href="/contact" style={{ color: "inherit", textDecoration: "none" }}>Contact</a></p>
       </div>
 
+      {/* Socials Section */}
       <div>
         <h4>Follow Us</h4>
-
-        <p>Instagram</p>
-
-        <p>Facebook</p>
-
-        <p>Twitter</p>
+        <p><a href="https://instagram.com" target="_blank" rel="noreferrer" style={{ color: "inherit", textDecoration: "none" }}>Instagram</a></p>
+        <p><a href="https://facebook.com" target="_blank" rel="noreferrer" style={{ color: "inherit", textDecoration: "none" }}>Facebook</a></p>
+        <p><a href="https://twitter.com" target="_blank" rel="noreferrer" style={{ color: "inherit", textDecoration: "none" }}>Twitter</a></p>
       </div>
 
     </footer>
